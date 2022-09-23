981af779b4d5ae3749f227e394aebb6d92a959c0
elizavetacher44@gmail.com <elizavetacher44@gmail.com>
2022-09-23 14:23:45 +1000

    задание 4.4 третье окно с "перемещающимеся" панелями

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -39,5 +39,10 @@ namespace YP2Cherednichenko
             Window2 window2 = new Window2();
             window2.Show();
         }
+        private void Win3_Click(object sender, RoutedEventArgs e)
+        {
+            Window3 window3 = new Window3();
+            window3.Show();
+        }
     }
 }
